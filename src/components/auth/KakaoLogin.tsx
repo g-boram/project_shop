d9ee97c827333a4b884f1b80db066f9df58ee564
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-04-15 15:56:14 +0900

    카카오 로딩 박스 수정

--- src/components/auth/KakaoLogin.tsx
+++ src/components/auth/KakaoLogin.tsx
@@ -141,7 +141,7 @@ export default function KakaoLogin() {
 
 const KakaoLoadingBox = styled.div`
   height: 100vh;
-  width: 100vw;
+  width: 100%;
   display: flex;
   flex-direction: column;
   align-items: center;
